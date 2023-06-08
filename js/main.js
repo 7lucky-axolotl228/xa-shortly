750306da2d8afd572f25a7e873e774db60779c34
7lucky-axolotl228 <7error228@gmail.com>
2023-06-08 14:13:13 +0500

    Add js styles to copy buttons

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 const elUrlShortenerForm = document.querySelector('.js-url-shortener-form');
 const elUrlShortenerResults = document.querySelector('.url-shortener__results');
 
+if (elUrlShortenerForm) {
+  elUrlShortenerForm.addEventListener('submit', function(evt) {
+    evt.preventDefault();
 
-elUrlShortenerForm.addEventListener('submit', function(evt) {
-  evt.preventDefault();
+    elUrlShortenerResults.classList.add('url-shortener__results--open');
+  });
+}
 
-  elUrlShortenerResults.classList.add('url-shortener__results--open');
-});
+
+
+const elCopyUrlShortLinkButton = document.querySelector('.js-copy-short-link-button');
+const elUrlShortener = document.querySelector('.url-shortener');
+
+if (elUrlShortener) {
+  elUrlShortener.addEventListener('click', function(evt) {
+    if (evt.target.matches('.js-copy-short-link-button')) {
+      /* Change button text */
+      evt.target.textContent = 'Copied!';
+      /* Change button bgcolor */
+      evt.target.classList.add('btn-black');
+      /* Reset button text and class*/
+      setTimeout(function () {
+        evt.target.textContent = 'Copy';
+        evt.target.classList.remove('btn-black');
+      }, 1400);
+    }
+  });
+}
